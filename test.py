e65b724dbded5abccce4db7aa90364c860beeb55
Seyon Shanthan <seyontennis@gmail.com>
2023-11-01 20:16:31 +0100

    new shield

--- test.py
+++ test.py
@@ -2,3 +2,18 @@
 
 #my new feature branch
 print("my feature branch")
+
+class Shield:
+    def __init__(self, health):
+        self.health = health
+    def add(self, added):
+        self.health += added
+
+shield = Shield(100)
+
+shield.add(20)
+
+print(shield.health)
+
+
+        
